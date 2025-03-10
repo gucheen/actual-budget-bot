3727eb2832cdaf57ff7052d9072a7946437dc461
gucheen <guchengf@gmail.com>
2025-03-10 12:10:06 +0800

    feat: 重构逻辑，支持同天同账户同金额的多条交易对账

--- src/reconcil.ts
+++ src/reconcil.ts
@@ -42,6 +42,10 @@ try {
   console.log('no mappings')
 }
 
+function createKeyForTransaction(transaction: Transaction) {
+  return `${transaction.date}_${transaction.account}_${transaction.amount}`
+}
+
 async function initActual() {
   await actualApi.init({
     dataDir: './actual-data',
@@ -62,164 +66,139 @@ async function getActualAccountsNameIdMap(): Promise<{ [key: string]: UUID }> {
   }, {} as { [key: string]: UUID })
 }
 
-async function readAlipayOriginalCSV(csvFilePath: string): Promise<{ [key: string]: string }[]> {
+async function readCSV(filePath: string, options: { skipLines: number; logMessage: string }): Promise<{ [key: string]: string }[]> {
   try {
-    await fs.promises.access(csvFilePath)
+    await fs.promises.access(filePath)
   } catch (error) {
     console.error('csv 文件不存在')
     process.exit(1)
   }
   return new Promise((resolve) => {
     const results: any[] = []
-    fs.createReadStream(csvFilePath)
-      .pipe(csv({
-        // 支付宝导出个人对账单，前 24 行是注释说明
-        skipLines: 24,
-      }))
+    fs.createReadStream(filePath)
+      .pipe(csv({ skipLines: options.skipLines }))
       .on('data', data => results.push(data))
       .on('end', () => {
-        console.log(`支付宝账单共 ${results.length} 条数据`)
+        console.log(options.logMessage.replace('${length}', results.length.toString()))
         resolve(results)
       })
   })
 }
 
 async function reconcilAlipayBills(billFilePath: string): Promise<{ unmatched: any[], unReconcilData: any[] }> {
-  const data = await readAlipayOriginalCSV(billFilePath)
-
-  await initActual()
-
-  const accountsMap = await getActualAccountsNameIdMap()
-
-  const endDate = data[0]['交易时间'].substring(0, 10)
-  const startDate = data[data.length - 1]['交易时间'].substring(0, 10)
-
-  console.log(`支付宝账单日期范围：${startDate} ~ ${endDate}`)
-  const transactions: Transaction[] = (await runQuery(q('transactions').filter({ date: { $gte: startDate, $lte: endDate } }).select('*' as any)) as any).data
-
-  console.log(`actual budget 对应日期范围共 ${transactions.length} 条交易`)
-
-  const unReconcilData: any[] = []
-  const unmatched: { [key: string]: string }[] = data.filter(item => {
-    if (item['收/付款方式'] === '' || item['交易分类'] === '转账红包') {
-      // 没有收付款方式一般是天猫、淘宝的各种充值金账户（比如猫超卡）
-      // 转账红包暂时不对账
-      unReconcilData.push(item)
+  return reconcilBills(billFilePath, {
+    // 支付宝导出个人对账单，前 24 行是注释说明
+    readCSVOptions: { skipLines: 24, logMessage: '支付宝账单共 ${length} 条数据' },
+    filterUnreconciled: item => {
+      if (item['收/付款方式'] === '' || item['交易分类'] === '转账红包') {
+        // 没有收付款方式一般是天猫、淘宝的各种充值金账户（比如猫超卡）
+        // 转账红包暂时不对账
+        return true
+      }
+      if (item['交易分类'] === '投资理财' && item['交易对方'] === '余额宝' && item['收/付款方式'] === '账户余额') {
+        // 支付宝账户余额操作的余额宝买入、卖出暂时不对账
+        return true
+      }
       return false
-    }
-    if (item['交易分类'] === '投资理财' && item['交易对方'] === '余额宝' && item['收/付款方式'] === '账户余额') {
-      // 支付宝账户余额操作的余额宝买入、卖出暂时不对账
-      unReconcilData.push(item)
-      return false
-    }
-    let originalAccountName = item['收/付款方式']
-    if (originalAccountName.includes('&')) {
-      // 导出的支付方式会用 & 分隔，第一个是真正的付款方式，后面是各种优惠活动
-      originalAccountName = originalAccountName.split('&')[0]
-    }
-    const accountName = ACCOUNT_NAME_MAP[originalAccountName] || originalAccountName
-    const accountId = accountsMap[accountName]
-    let amount = actualApi.utils.amountToInteger(Number(item['金额']))
-    if (item['收/支'] === '支出') {
-      amount = -amount
-    } else if (item['收/支'] === '不计收支' && item['交易分类'] === '投资理财' && item['商品说明'].includes('买入')) {
-      // 投资买入在 actual 中记作转账，对于转出账户来说是支出，买入的基金等对账暂时不处理
-      amount = -amount
-    }
-    const date = item['交易时间'].substring(0, 10)
-    const matchTrans = transactions.find((transaction: Transaction) => {
-      // 日期、金额、账户一致认为是匹配的
-      // 存在特殊情况，比如一天多笔相同的支付，暂时不处理
-      return transaction.date === date && transaction.amount === amount && transaction.account === accountId
-    })
-
-    const findMatch = typeof matchTrans !== 'undefined'
-
-    // 还未标记清算的交易，更新为已清算
-    if (findMatch && !matchTrans.cleared) {
-      actualApi.updateTransaction(matchTrans.id, { cleared: true })
-    }
-
-    return !findMatch
-  })
-
-  await actualApi.shutdown()
-
-  return {
-    unmatched,
-    unReconcilData,
-  }
-}
-
-async function readWechatOriginalCSV(csvFilePath: string): Promise<{ [key: string]: string }[]> {
-  try {
-    await fs.promises.access(csvFilePath)
-  } catch (error) {
-    console.error('csv 文件不存在')
-    process.exit(1)
-  }
-  return new Promise((resolve) => {
-    const results: any[] = []
-    fs.createReadStream(csvFilePath)
-      .pipe(csv({
-        // 微信导出个人对账单，前 16 行是注释说明
-        skipLines: 16,
-      }))
-      .on('data', data => results.push(data))
-      .on('end', () => {
-        console.log(`微信支付账单共 ${results.length} 条数据`)
-        resolve(results)
-      })
+    },
+    getAccountName: item => {
+      let originalAccountName = item['收/付款方式']
+      if (originalAccountName.includes('&')) {
+        // 导出的支付方式会用 & 分隔，第一个是真正的付款方式，后面是各种优惠活动
+        originalAccountName = originalAccountName.split('&')[0]
+      }
+      const accountName = ACCOUNT_NAME_MAP[originalAccountName] || originalAccountName
+      return accountName
+    },
+    getAmount: item => {
+      let amount = Number(item['金额'])
+      if (item['收/支'] === '支出') {
+        amount = -amount
+      } else if (item['收/支'] === '不计收支' && item['交易分类'] === '投资理财' && item['商品说明'].includes('买入')) {
+        // 投资买入在 actual 中记作转账，对于转出账户来说是支出，买入的基金等对账暂时不处理
+        amount = -amount
+      }
+      return amount
+    },
   })
 }
 
 async function reconcilWechatBills(billFilePath: string): Promise<{ unmatched: any[], unReconcilData: any[] }> {
-  const data = await readWechatOriginalCSV(billFilePath)
+  return reconcilBills(billFilePath, {
+    // 微信导出个人对账单，前 16 行是注释说明
+    readCSVOptions: { skipLines: 16, logMessage: '微信支付账单共 ${length} 条数据' },
+    getAccountName: item => {
+      const originalAccountName = item['支付方式']
+      const accountName = ACCOUNT_NAME_MAP[originalAccountName] || originalAccountName
+      return accountName
+    },
+    getAmount: item => {
+      let amount = Number(item['金额(元)'].replace('¥', ''))
+      if (item['收/支'] === '支出') {
+        amount = -amount
+      }
+      return amount
+    },
+  })
+}
 
+async function reconcilBills(
+  billFilePath: string,
+  processor: {
+    readCSVOptions: { skipLines: number; logMessage: string };
+    getAccountName: (item: any) => string;
+    getAmount: (item: any) => number;
+    // 过滤掉不需要对账的交易
+    filterUnreconciled?: (item: any) => boolean;
+  }
+): Promise<{ unmatched: any[], unReconcilData: any[] }> {
+  const data = await readCSV(billFilePath, processor.readCSVOptions)
   await initActual()
-
   const accountsMap = await getActualAccountsNameIdMap()
 
   const endDate = data[0]['交易时间'].substring(0, 10)
   const startDate = data[data.length - 1]['交易时间'].substring(0, 10)
 
-  console.log(`微信支付账单日期范围：${startDate} ~ ${endDate}`)
+  console.log(`账单日期范围：${startDate} ~ ${endDate}`)
   const transactions: Transaction[] = (await runQuery(q('transactions').filter({ date: { $gte: startDate, $lte: endDate } }).select('*' as any)) as any).data
-
   console.log(`actual budget 对应日期范围共 ${transactions.length} 条交易`)
 
+  // 这个map用来记录同一天多笔相同支付账户和金额的交易
+  const multipleTransactionIndex = new Map<string, number>()
   const unReconcilData: any[] = []
-  const unmatched: { [key: string]: string }[] = data.filter(item => {
-    const originalAccountName = item['支付方式']
-    const accountName = ACCOUNT_NAME_MAP[originalAccountName] || originalAccountName
-    const accountId = accountsMap[accountName]
-    let amount = actualApi.utils.amountToInteger(Number(item['金额(元)'].replace('¥', '')))
-    if (item['收/支'] === '支出') {
-      amount = -amount
+  const unmatched = data.filter(item => {
+    if (typeof processor.filterUnreconciled === 'function' && processor.filterUnreconciled(item)) {
+      unReconcilData.push(item)
+      return false
     }
+
+    const accountId = accountsMap[processor.getAccountName(item)]
+    const amount = actualApi.utils.amountToInteger(processor.getAmount(item))
     const date = item['交易时间'].substring(0, 10)
-    const matchTrans = transactions.find((transaction: Transaction) => {
+
+    const matchTrans = transactions.filter(t =>
       // 日期、金额、账户一致认为是匹配的
-      // 存在特殊情况，比如一天多笔相同的支付，暂时不处理
-      return transaction.date === date && transaction.amount === amount && transaction.account === accountId
-    })
+      t.date === date && t.amount === amount && t.account === accountId
+    )
 
-    const findMatch = typeof matchTrans !== 'undefined'
-
-    // 还未标记清算的交易，更新为已清算
-    if (findMatch && !matchTrans.cleared) {
-      actualApi.updateTransaction(matchTrans.id, { cleared: true })
+    let index = 0
+    if (matchTrans.length > 1) {
+      // 同一天多笔相同支付账户和金额的交易
+      const key = createKeyForTransaction({ date, account: accountId, amount })
+      index = multipleTransactionIndex.get(key) || 0
+      multipleTransactionIndex.set(key, index + 1)
     }
 
-    return !findMatch
+    const matchTransaction = matchTrans[index]
+    if (matchTransaction && !matchTransaction.cleared) {
+      actualApi.updateTransaction(matchTransaction.id, { cleared: true })
+    }
+
+    return !matchTransaction
   })
 
   await actualApi.shutdown()
-
-  return {
-    unmatched,
-    unReconcilData,
-  }
+  return { unmatched, unReconcilData }
 }
 
 async function dealReconcilResults(results: { unmatched: any[], unReconcilData: any[] }) {
