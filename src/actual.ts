9052676bcedc20b7728f56400b56d326bce6dfa0
gucheen <guchengf@gmail.com>
2025-03-10 10:03:02 +0800

    feat: 有importedID的情况下才判断是否存在重复交易

--- src/actual.ts
+++ src/actual.ts
@@ -65,10 +65,12 @@ export const addActualTransaction = async (trasnactionData: {
 
   console.log('matchAccount', matchAccount)
 
-  const dulplicatedTransaction = await runQuery(q('transactions').filter({ account: matchAccount.id, imported_id: importedID }).select(['*'])) as { data: Transaction[], dependencies: string[] }
-  if (dulplicatedTransaction && Array.isArray(dulplicatedTransaction.data) && dulplicatedTransaction.data.length > 0) {
-    console.log('dulplicatedTransaction', dulplicatedTransaction)
-    throw new Error(`已经存在相同的交易，imported_id：${importedID}`)
+  if (importedID) {
+    const dulplicatedTransaction = await runQuery(q('transactions').filter({ account: matchAccount.id, imported_id: importedID }).select(['*'])) as { data: Transaction[], dependencies: string[] }
+    if (dulplicatedTransaction && Array.isArray(dulplicatedTransaction.data) && dulplicatedTransaction.data.length > 0) {
+      console.log('dulplicatedTransaction', dulplicatedTransaction)
+      throw new Error(`已经存在相同的交易，imported_id：${importedID}`)
+    }
   }
 
   const transaction = {
